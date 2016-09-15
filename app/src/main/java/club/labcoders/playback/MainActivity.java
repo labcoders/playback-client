acf66ecc3f338abec48522bc60e9fd515377b1dc
Jacob Errington <git@mail.jerrington.me>
2016-09-15 15:01:39 -0400

    upgrade to AS2.2-RC2, implement ABQ linearization, ABQ GC
    State of the app: We have a single activity that displays the state of the
    RecordingService and that shows a button labelled "record". The record button
    causes a serialization of the ABQ and emits a toast showing how many samples
    are contained in the linearization.
    
    Definitions:
    
      * Audio Buffer Queue (ABQ) - the internal FIFO queue of the RecordingService
        that holds chunks emitted by the observable connected to the microphone.
      * chunk - a chunk is a small buffer of samples (a short[]) that holds about
        40ms of raw audio.
      * chunk-second - a unit of time that represents the average duration of a
        chunk, in seconds.
      * linearization - the process that converts the contents of the ABQ into a
        single buffer of samples called a snapshot. The linearization process
        performs an ABQ GC in order to discard too-old chunks in the ABQ. If this
        GC were not performed, then the recording would not capture *exactly* the
        last 20 or 60 or whatever seconds of audio; it would capture the audio from
        20-P seconds ago to P seconds ago, where P is on average one half of the GC
        period.
      * snapshot - the desired output of our system when the record button is
        pressed: the last N (accurate to within +- one chunk) seconds of audio
        heard by the microphone becomes a snapshot. Linearization of the ABQ
        produces a *raw* snapshot, which is then to be encoded into an *encoded*
        snapshot. This is then to be sent to the web service.
      * ABQ GC: the observable connected to the microphone has a subscriber
        registered to it when the RecordingService is started. This
        observable/subscriber relationship forms a tight loop that polls the
        microphone and appends the buffer to the queue. The queue has built-in
        concurrency support and is wait-free, so no locks are necessary to write to
        the queue. However, this means the queue will grow until a linearization
        takes place, but linearization occurs only upon user request. This means
        that another process is required to remove old chunks from the ABQ: that
        process is the ABQ GC. The GC currently is set to run every second, and
        will delete as many chunks as necessary in order to keep the length of the
        ABQ in seconds to within one chunk-second of the configured ABQ duration.

--- app/src/main/java/club/labcoders/playback/MainActivity.java
+++ app/src/main/java/club/labcoders/playback/MainActivity.java
@@ -4,40 +4,123 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.media.AudioRecord;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import rx.subscriptions.CompositeSubscription;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.statusText)
+    TextView statusText;
+
+    @BindView(R.id.recordButton)
+    Button recordButton;
+
+    RecordingService recordingService;
+    private boolean recordingServiceIsBound;
+    private CompositeSubscription subscriptions;
+
+    public MainActivity() {
+        recordingServiceIsBound = false;
+    }
+
+    @OnClick(R.id.recordButton)
+    public void onRecordButtonClick() {
+        if(recordingService == null) {
+            Toast.makeText(this, "No recording service.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            final Subscription sub = Observable.defer(() -> Observable.just(recordingService.getBufferedAudio()))
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(audio -> {
+                        Toast.makeText(
+                                this,
+                                String.format("Recorded %d samples.", audio.length),
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    });
+            subscriptions.add(sub);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        subscriptions = new CompositeSubscription();
+        setContentView(R.layout.activity_main);
+        ButterKnife.setDebug(true);
+        ButterKnife.bind(this);
 
         Timber.plant(new Timber.DebugTree());
 
-        setContentView(R.layout.activity_main);
+        Intent recordingIntent = new Intent(this, RecordingService.class);
+        startService(recordingIntent);
+        Timber.d("Started recording service.");
+        bindService(recordingIntent, connection, Context.BIND_IMPORTANT);
+        Timber.d("Bound recording service.");
+        recordingServiceIsBound = true;
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
-        Intent recordingIntent = new Intent(this, RecordingService.class);
-        startService(recordingIntent);
-        bindService(recordingIntent, connection, Context.BIND_IMPORTANT);
     }
 
-    ServiceConnection connection = new ServiceConnection() {
+    @Override
+    protected void onDestroy() {
+        if(recordingServiceIsBound) {
+            unbindService(connection);
+            recordingServiceIsBound = false;
+        }
+        subscriptions.unsubscribe();
+        super.onDestroy();
+    }
+
+    final ServiceConnection connection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
+            final RecordingService.RecordingServiceBinder binder = (RecordingService.RecordingServiceBinder)service;
+            recordingService = binder.getService();
 
+            final Subscription sub = recordingService.recordingState
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(
+                            state -> {
+                                if(state == AudioRecord.RECORDSTATE_RECORDING) {
+                                    statusText.setText(R.string.statusRecording);
+                                }
+                                else {
+                                    statusText.setText(R.string.statusNotRecording);
+                                }
+                            },
+                            error -> {
+                                statusText.setText(error.toString());
+                            },
+                            () -> {
+                                statusText.setText(R.string.statusPlaceholder);
+                            }
+                    );
+            subscriptions.add(sub);
         }
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
-
+            recordingService = null;
         }
-    }
+    };
 }
--- app/src/main/java/club/labcoders/playback/RecordingService.java
+++ app/src/main/java/club/labcoders/playback/RecordingService.java
@@ -6,69 +6,302 @@ import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Binder;
-import android.os.IBinder;
-import android.os.IInterface;
-import android.os.Parcel;
-import android.os.RemoteException;
-import android.widget.Toast;
 
-import java.io.FileDescriptor;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
+import rx.Observable;
+import rx.Subscription;
+import rx.schedulers.Schedulers;
+import rx.subscriptions.CompositeSubscription;
+import timber.log.Timber;
 
 public class RecordingService extends Service {
+    /**
+     * Milliseconds between runs of the buffer GC, which removes chunks from
+     * the {@link #audioBufferQueue}.
+     */
+    public static final long GC_PERIOD = 1000;
+
+    /**
+     * Milliseconds between emissions by the {@link #recordingState} observable.
+     */
+    public static final long STATE_POLL_PERIOD = 250;
+
+    /**
+     * Number of seconds in a record snapshot.
+     * <p>
+     * TODO: make this a configurable parameter of the application.
+     */
+    public static final int DEFAULT_CHUNK_LENGTH = 10;
     AudioRecord audioRecord;
-    RecordingServiceWorker worker;
-    static final int SAMPLE_RATE = 44100;
-    final int bufferSize;
-    volatile CircularShortBuffer mainBuffer;
+
+    /**
+     * Sample rate of the audio recording.
+     * <p>
+     * 44.1kHz 16-bit PCM is a standard supported by all Android devices.
+     */
+    public static final int SAMPLE_RATE = 44100;
+
+    /**
+     * The size of the audio buffers used internally by Android in our audio
+     * recorder.
+     */
+    public final int bufferSize;
+
+    /**
+     * The size of the buffer that we fill when polling the microphone.
+     * <p>
+     * Remark: per the docs, this must be *smaller* than {@link #bufferSize}!
+     */
+    public final int chunkBufferSize;
+
+    /**
+     * A hot observable connected to the microphone. This observable obtains
+     * a value when the service is started. It emits buffers containing the
+     * raw samples read from the microphone.
+     */
+    public final Observable<short[]> audioStream;
+
+    /**
+     * A hot observable connected to the microphone. It emits the recording
+     * state of the microphone every {@link #STATE_POLL_PERIOD} milliseconds.
+     */
+    public final Observable<Integer> recordingState;
+
+    /**
+     * The duration, in seconds, of an audio snapshot created by
+     * {@link #getBufferedAudio()}.
+     */
+    public final int snapshotLengthSeconds;
+
+    /**
+     * The duration, in samples, of an audio snapshot created by
+     * {@link #getBufferedAudio()}.
+     */
+    public final int snapshotLengthSamples;
+
+    /**
+     * The queue used internally to hold buffers emitted by the
+     * {@link #audioStream} observable. The internal garbage collector
+     * periodically pops as many buffers as necessary from this queue in
+     * order to keep the length of the queue below
+     * {@link #maxAudioBufferQueueLength}.
+     */
+    public final Queue<short[]> audioBufferQueue;
+
+    /**
+     * A lock used for read access to the queue.
+     *
+     * The queue is wait-free. The microphone can continue to push data onto
+     * it as much as it likes, since it is the only producer for it. However,
+     * there are two consumers for the queue. One consumer is the garbage
+     * collection process that drops old buffers when the queue's length
+     * exceeds the configured duration. Another consumer begins when the
+     * linearization process occurs. Consequently, when linearization is
+     * occurring, we do not want the garbage collection process to run, as
+     * that process may drop some of the buffers that we have yet to linearize.
+     *
+     * {@see #queueGc()}
+     * {@see #audioBufferQueue}
+     */
+    public final ReentrantLock queueLock;
+
+    /**
+     * Approximates the length of the audioBufferQueue. The queue is no shorter
+     * than this amount at any given time.
+     */
+    private AtomicInteger audioBufferQueueLength;
+
+    /**
+     * The maximum length of the audio buffer queue, at which point it holds
+     * approximately (within <50ms on most devices)
+     * {@link #snapshotLengthSeconds} seconds of audio.
+     */
+    public final int maxAudioBufferQueueLength;
+
+    private final CompositeSubscription subscriptions;
+
 
     public RecordingService() {
+        snapshotLengthSeconds = DEFAULT_CHUNK_LENGTH;
+        snapshotLengthSamples = SAMPLE_RATE * snapshotLengthSeconds;
+        audioBufferQueue = new ConcurrentLinkedQueue<>();
+        audioBufferQueueLength = new AtomicInteger(0);
+        queueLock = new ReentrantLock(true);
+        subscriptions = new CompositeSubscription();
+
         bufferSize = AudioRecord.getMinBufferSize(
                 SAMPLE_RATE,
                 AudioFormat.CHANNEL_IN_MONO,
                 AudioFormat.ENCODING_PCM_16BIT
         );
+        chunkBufferSize = bufferSize / 2;
+        maxAudioBufferQueueLength = snapshotLengthSamples / chunkBufferSize;
+
+        audioStream = Observable.create(subscriber -> {
+            initAudioRecord();
+
+            if (audioRecord.getState() != AudioRecord.STATE_INITIALIZED) {
+                subscriber.onError(new RuntimeException("Failed to initialize" +
+                        " AudioRecord"));
+                return;
+            }
+
+            audioRecord.startRecording();
+            while (audioRecord.getRecordingState() == AudioRecord
+                    .RECORDSTATE_RECORDING) {
+                short[] audioBuffer = new short[bufferSize / 2];
+                audioRecord.read(audioBuffer, 0, audioBuffer.length);
+                subscriber.onNext(audioBuffer);
+            }
+
+            subscriber.onCompleted();
+        });
+
+        recordingState = Observable.create(subscriber -> {
+            while (audioRecord.getState() != AudioRecord.STATE_UNINITIALIZED) {
+                subscriber.onNext(audioRecord.getRecordingState());
+                try {
+                    Thread.sleep(STATE_POLL_PERIOD);
+                } catch (InterruptedException e) {
+                    subscriber.onError(e);
+                }
+            }
+            Timber.d("Recording state monitor died.");
+            subscriber.onCompleted();
+        });
     }
 
-    @Override
-    public void onCreate() {
-        audioRecord = new AudioRecord(
-                MediaRecorder.AudioSource.MIC,
-                SAMPLE_RATE,
-                AudioFormat.CHANNEL_IN_MONO,
-                AudioFormat.ENCODING_PCM_16BIT,
-                bufferSize
-        );
+    /**
+     * Merges a given number of linked buffers in the audioBufferQueue into a
+     * single array. New buffers may be added to the queue concurrently;
+     * these buffers will not be added to the queue.
+     *
+     * @return A single array with the merged contents of all the linked
+     * buffers in the audioBufferQueue.
+     */
+    private short[] linearizeQueue(final int maxMergedBuffers) {
+        short[] result = new short[maxMergedBuffers * chunkBufferSize];
+        int processedBufferCount = 0;
+        Timber.d("Starting audio buffer queue linearization.");
+        try {
+            Timber.d("Locking audio buffer queue.");
+            queueLock.lock();
+            final long startTime = System.nanoTime();
+            Timber.d("Doing pre-linearization GC.");
+            queueGc();
+            for (short[] buffer : audioBufferQueue) {
+                if (processedBufferCount == maxMergedBuffers)
+                    break;
+                final int writeOffset = processedBufferCount * chunkBufferSize;
+                System.arraycopy(buffer, 0, result, writeOffset,
+                        chunkBufferSize);
+            }
+            final long endTime = System.nanoTime();
+            final long delta = (endTime - startTime) / (long) 1e3; // microseconds
+
+            Timber.d("Audio buffer queue linearized in %d microseconds.",
+                    delta);
+        } finally {
+            if (queueLock.isHeldByCurrentThread())
+                queueLock.unlock();
+        }
+        return result;
     }
 
-    @Override
-    public void onStart(Intent intent, int flags) {
-        handleCommand(intent);
+    /**
+     * Returns a buffer of the last {@link #snapshotLengthSeconds} seconds of
+     * audio.
+     *
+     * @return The recorded audio, if any.
+     */
+    public short[] getBufferedAudio() {
+        // snapshotLengthSamples is the number of samples that make up a
+        // piece of
+        // audio with length snapshotLengthSeconds; dividing by chunkBufferSize
+        // gives us the number of buffers we need to pop to get a sample
+        // array whose represented audio has that length in seconds.
+        return linearizeQueue(maxAudioBufferQueueLength);
+    }
+
+    /**
+     * Initializes the {@link AudioRecord} object internal to the
+     * RecordingService.
+     */
+    private void initAudioRecord() {
+        if (audioRecord == null)
+            audioRecord = new AudioRecord(
+                    MediaRecorder.AudioSource.MIC,
+                    SAMPLE_RATE,
+                    AudioFormat.CHANNEL_IN_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    bufferSize
+            );
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        handleCommand(intent);
+        final Subscription producer = audioStream
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.io())
+                .subscribe(shorts -> {
+                    audioBufferQueue.add(shorts);
+                    final int count = audioBufferQueueLength.incrementAndGet();
+                });
+        subscriptions.add(producer);
+
+        final Observable<Void> gcTimer = Observable.create(subscriber -> {
+            while (true) {
+                subscriber.onNext(null);
+                try {
+                    Thread.sleep(GC_PERIOD);
+                } catch (InterruptedException e) {
+                    subscriber.onError(e);
+                    return;
+                }
+            }
+        });
+        subscriptions.add(
+                gcTimer.subscribeOn(Schedulers.io())
+                        .observeOn(Schedulers.io())
+                        .subscribe(aVoid -> queueGc())
+        );
         return START_STICKY;
     }
 
-    private synchronized void handleCommand(Intent intent) {
-        if(audioRecord.getState() != AudioRecord.STATE_INITIALIZED)
-            throw new RuntimeException("Failed to initialize audio recorder.");
-        if(audioRecord.getRecordingState() == AudioRecord.RECORDSTATE_RECORDING)
-            return;
-        audioRecord.startRecording();
-        worker = new RecordingServiceWorker();
-        worker.start();
-        Toast.makeText(this, "Started listener service.", Toast.LENGTH_SHORT).show();
-    }
-
-    public boolean isRecording() {
-        return worker != null && worker.stillRunning;
+    /**
+     * Perform a garbage collection of the audio buffer queue.
+     * <p>
+     * This will acquire the queue lock, so that other garbage collection
+     * processes or linearization processes cannot be executing concurrently.
+     */
+    private void queueGc() {
+        int collected = 0;
+        while (audioBufferQueueLength.get() >
+                maxAudioBufferQueueLength) {
+            try {
+                queueLock.lock();
+                audioBufferQueue.remove();
+                audioBufferQueueLength.decrementAndGet();
+                collected++;
+            } finally {
+                queueLock.unlock();
+            }
+        }
+        if (collected > 0) {
+            Timber.d("Garbage collected %d chunks.",
+                    collected);
+        }
     }
 
     @Override
     public void onDestroy() {
-        worker.pleaseStop();
+        subscriptions.unsubscribe();
+        audioRecord.stop();
+        audioRecord.release();
     }
 
     @Override
@@ -77,38 +310,8 @@ public class RecordingService extends Service {
     }
 
     class RecordingServiceBinder extends Binder {
-        public RecordingService getService() {
+        RecordingService getService() {
             return RecordingService.this;
-        }
-    }
-
-    public class RecordingServiceWorker extends Thread {
-        volatile boolean stillRunning;
-
-        public RecordingServiceWorker() {
-            this.stillRunning = true;
-        }
-
-        public void pleaseStop() {
-            stillRunning = false;
-        }
-
-        @Override
-        public void run() {
-            try {
-                mainLoop();
-            }
-            finally {
-                audioRecord.stop();
-            }
-        }
-
-        private void mainLoop() {
-            short[] audioBuffer = new short[bufferSize / 2];
-            while(stillRunning) {
-                audioRecord.read(audioBuffer, 0, audioBuffer.length);
-
-            }
         }
     }
 }
