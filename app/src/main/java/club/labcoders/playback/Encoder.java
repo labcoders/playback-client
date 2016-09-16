d6e2aed98a06f99f08d13d4e2f8a3acbb2c8d762
Kevin Li <github@mail.kevinl.io>
2016-09-16 16:52:52 -0400

    encoder and use it

--- app/src/main/java/club/labcoders/playback/Encoder.java
+++ app/src/main/java/club/labcoders/playback/Encoder.java
@@ -3,7 +3,10 @@ package club.labcoders.playback;
 import android.media.MediaCodec;
 import android.media.MediaFormat;
 
+import java.io.ByteArrayOutputStream;
 import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.Queue;
 
 import club.labcoders.playback.concurrent.CVar;
 import rx.Observable;
@@ -18,61 +21,90 @@ public class Encoder implements Observable.Operator<byte[], byte[]> {
 
     boolean hasError = false;
     Throwable error = null;
+
     private boolean finished;
+    private boolean codecStarted;
 
     public Encoder(final MediaCodec configuredCodec) {
         codec = configuredCodec;
         finished = false;
+        codecStarted = false;
+    }
+
+    public class ExposedSubscriber  extends Subscriber<byte[]> {
+        private Subscriber<? super byte[]> subscriber;
+        private CVar<Integer> bufferIndex;
+
+        public ExposedSubscriber(Subscriber<? super byte[]> sub, CVar<Integer> cvar) {
+            subscriber = sub;
+            bufferIndex = cvar;
+        }
+
+        public Subscriber<? super byte[]> getSubscriber() {
+            return subscriber;
+        }
+
+        @Override
+        public void onCompleted() {
+            try {
+                int idx = bufferIndex.read();
+                codec.queueInputBuffer(idx, 0, 0, 0, codec.BUFFER_FLAG_END_OF_STREAM);
+            } catch (InterruptedException e) {
+                onError(e);
+            }
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            if(!subscriber.isUnsubscribed()) {
+                subscriber.onError(e);
+            }
+        }
+
+        @Override
+        public void onNext(byte[] bytes) {
+            if (!codecStarted) {
+                codec.start();
+                codecStarted = true;
+            }
+            byte[] encoded = null;
+            if(subscriber.isUnsubscribed()) {
+                return;
+            }
+            try {
+                int pointer = 0;
+                while (pointer < bytes.length) {
+                    int idx = bufferIndex.read();
+                    ByteBuffer buffer = codec.getInputBuffer(idx);
+                    int capacity = buffer.capacity();
+                    int inc = Math.min(bytes.length - pointer, capacity);
+
+                    buffer.put(bytes, pointer, inc);
+                    codec.queueInputBuffer(idx, 0, inc, 0, 0);
+
+                    pointer += inc;
+                }
+            } catch (InterruptedException e) {
+                onError(e);
+            }
+        }
     }
 
     @Override
     public Subscriber<? super byte[]>
     call(Subscriber<? super byte[]> subscriber) {
         MediaCodec.Callback callback;
-        final CVar<byte[]> encodeIn = new CVar<>();
-        final CVar<byte[]> encodeOut = new CVar<>();
+        final CVar<Integer> bufferIndex = new CVar<Integer>();
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
 
-        final Subscriber<byte[]> sub =
-                new Subscriber<byte[]>(subscriber) {
-                    @Override
-                    public void onCompleted() {
-                        if(!subscriber.isUnsubscribed()) {
-                            subscriber.onCompleted();
-                        }
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        if(!subscriber.isUnsubscribed()) {
-                            subscriber.onError(e);
-                        }
-                    }
-
-                    @Override
-                    public void onNext(byte[] byteBuffer) {
-                        byte[] encoded = null;
-                        if(subscriber.isUnsubscribed())
-                            return;
-                        try {
-                            encodeIn.write(byteBuffer);
-                            encoded = encodeOut.read();
-                        } catch (InterruptedException e) {
-                            onError(e);
-                        }
-                        subscriber.onNext(encoded);
-                    }
-                };
+        ExposedSubscriber exp = new ExposedSubscriber(subscriber, bufferIndex);
 
         callback = new MediaCodec.Callback() {
             @Override
             public void onInputBufferAvailable(
                     MediaCodec codec, int index) {
-                final ByteBuffer inputBuffer = codec.getInputBuffer(index);
-                int flags = finished ? MediaCodec.BUFFER_FLAG_END_OF_STREAM : 0;
                 try {
-                    final byte[] raw = encodeIn.read();
-                    inputBuffer.put(raw);
-                    codec.queueInputBuffer(index, 0, raw.length, 0, 0);
+                    bufferIndex.write(index);
                 }
                 catch(InterruptedException e) {
                     Timber.e("HOLY SHIT");
@@ -86,7 +118,13 @@ public class Encoder implements Observable.Operator<byte[], byte[]> {
                     MediaCodec codec,
                     int index,
                     MediaCodec.BufferInfo info) {
-                final
+                if (0 != (info.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM)) {
+                    exp.getSubscriber().onNext(codec.getOutputBuffer(index).array());
+                    exp.getSubscriber().onCompleted();
+                    codec.stop();
+                } else {
+                    exp.getSubscriber().onNext(codec.getOutputBuffer(index).array());
+                }
             }
 
             @Override
@@ -106,6 +144,6 @@ public class Encoder implements Observable.Operator<byte[], byte[]> {
 
         codec.setCallback(callback);
 
-        return sub;
+        return exp;
     }
 }
--- app/src/main/java/club/labcoders/playback/MainActivity.java
+++ app/src/main/java/club/labcoders/playback/MainActivity.java
@@ -9,17 +9,23 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.media.AudioRecord;
+import android.media.MediaCodec;
+import android.media.MediaCodecList;
+import android.media.MediaFormat;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.joda.time.DateTime;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 
 import butterknife.BindView;
@@ -31,6 +37,7 @@ import club.labcoders.playback.api.models.AudioRecording;
 import club.labcoders.playback.api.models.Base64Blob;
 import club.labcoders.playback.api.models.GeographicalPosition;
 import club.labcoders.playback.api.models.Ping;
+import club.labcoders.playback.concurrent.CVar;
 import rx.Observable;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
@@ -39,6 +46,10 @@ import rx.subscriptions.CompositeSubscription;
 import timber.log.Timber;
 
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import static android.media.MediaFormat.KEY_BIT_RATE;
+import static android.media.MediaFormat.KEY_CHANNEL_COUNT;
+import static android.media.MediaFormat.KEY_MIME;
+import static android.media.MediaFormat.KEY_SAMPLE_RATE;
 
 public class MainActivity extends AppCompatActivity {
     private static final int FINE_LOCATION_PERMISSION_REQUEST = 0;
@@ -62,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.uploadButton)
-    public void onRecordButtonClick() {
+    public void onRecordButtonClick() throws IOException, InterruptedException {
         double length;
         Location loc;
         Base64Blob blob;
@@ -77,6 +88,19 @@ public class MainActivity extends AppCompatActivity {
         } else {
             short[] shorts = recordingService.getBufferedAudio();
 
+            String mimetype = MimeTypeMap.getSingleton().getMimeTypeFromExtension("ogg");
+
+            MediaCodec codec = MediaCodec.createEncoderByType(mimetype);
+            MediaFormat format = new MediaFormat();
+            format.setString(MediaFormat.KEY_MIME, mimetype);
+            format.setInteger(KEY_BIT_RATE, 128);
+            format.setInteger(KEY_SAMPLE_RATE, recordingService.SAMPLE_RATE);
+            format.setInteger(KEY_CHANNEL_COUNT, 1);
+
+            codec.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+
+            Encoder enc = new Encoder(codec);
+
             // Calculate length of audio
             length = shorts.length / recordingService.SAMPLE_RATE;
 
@@ -84,10 +108,31 @@ public class MainActivity extends AppCompatActivity {
             for (short s : shorts) {
                 buf.putShort(s);
             }
-            // TODO: Compress audio later.
+
+            byte[] rawAudio = buf.array();
+            ByteArrayOutputStream compressedAudio = new ByteArrayOutputStream():
+
+            CVar<Void> signal = new CVar();
+
+            Observable.just(rawAudio).lift(enc).doOnNext(compressed -> {
+                try {
+                    compressedAudio.write(compressed);
+                } catch (IOException e) {
+                    Timber.e("Fuck")
+                }
+            }).doOnCompleted(() -> {
+                try {
+                    signal.write(null);
+                } catch (InterruptedException e) {
+                    Timber.e("Jesus fucking christ")
+                }
+            });
+
+            // Resynchronize.
+            signal.read();
 
             // Construct blob.
-            blob = new Base64Blob(buf.array());
+            blob = new Base64Blob(compressedAudio.toByteArray());
 
             // Get current date
             date = DateTime.now();
